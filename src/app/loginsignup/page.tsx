677cfdd0683e0a8e426b396a15c5d1722fa629af
tathya <tathyaorigins@gmail.com>
2025-06-05 22:49:52 +0530

    Changed Login Page

--- src/app/loginsignup/page.tsx
+++ src/app/loginsignup/page.tsx
@@ -146,10 +146,7 @@ export default function LoginPage() {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
+            Glint AI
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
